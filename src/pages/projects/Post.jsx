f41a8ba94bfe71e493782c219f84d2edc8945bf9
JennyLee02 <107929895+JennyLee02@users.noreply.github.com>
2024-02-27 23:56:01 -0500

    feat: post description added.

--- src/pages/projects/Post.jsx
+++ src/pages/projects/Post.jsx
@@ -10,18 +10,33 @@ export const Post = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
-  
+  const renderDescription = (descriptionArray) => {
+    return descriptionArray.map((item, index) => (
+      <p key={index}>
+        {item.children.map((child, childIndex) => (
+          // Directly display the text content of each child
+          <React.Fragment key={childIndex}>{child.text}</React.Fragment>
+        ))}
+      </p>
+    ));
+  };
+
+  const descriptionContent = data?.data?.attributes?.description;
 
 
   return (
     <div className="project-detail-page">
         <h2 className='post-title'>{data.data.attributes.title}</h2>
+        <p className='sub-title'>{data.data.attributes.preview}</p>
         <div className='post-image-container'>
             <img
                 src={`http://localhost:1337${data.data.attributes.image.data[0].attributes.formats.small.url}`} 
                 alt={data.data.attributes.title}
                 className='post-image'
             />
+        </div>
+        <div className='description'>
+          {descriptionContent && renderDescription(descriptionContent)}
         </div>
 
 
