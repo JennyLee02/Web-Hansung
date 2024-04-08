1ddbd5b3bbe8977cfed2d08f5c9b15123a93adac
JennyLee02 <107929895+JennyLee02@users.noreply.github.com>
2024-04-08 04:00:40 -0400

    feat: kanane blueprint scoller added.

--- src/pages/products/kanane/Kanane_type.jsx
+++ src/pages/products/kanane/Kanane_type.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { MdArrowBackIos } from "react-icons/md";
 import { MdArrowForwardIos } from "react-icons/md";
+import blueprintData from '../../../data/kananeData.json';
+
 
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
@@ -9,42 +11,33 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import './kanane_type.css';
 
-import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+import { EffectCoverflow, Pagination, Navigation} from 'swiper/modules';
 
-
-
-const roomData = [
-    {
-      name: 'Extended',
-      blueprint: '../../../images/Kanane/extend.png',
-    },
-    {
-      name: 'Terrace1',
-      blueprint: '../../../images/Kanane/terrace.png',
-    },
-    {
-      name: 'Terrace2',
-      blueprint: '../../../images/Kanane/terrace2.png',
-    },
-    {
-        name: 'Two Room',
-        blueprint: '../../../images/Kanane/tworoom.png',
-    },
-    {
-        name: 'Master Room',
-        blueprint: '../../../images/Kanane/masterroom.png',
-    }
-  ];
 
   const tabs = ['Extended', 'Terrace1','Terrace2', 'Two Room', 'Master Room'];
 
 
 const Kanane_type = () => {
-  
+    const [activeSlide, setActiveSlide] = useState(blueprintData[2]);
+
+    const handleSlideChange = (swiper) => {
+        setActiveSlide(blueprintData[swiper.realIndex]);
+      };
+
+
+    const slides = [
+        {name: 'Extended', blueprint: '/images/Kanane/extend.png'},
+        {name: 'Terrace1', blueprint: '/images/Kanane/terrace.png'},
+        {name: 'Terrace2', blueprint: '/images/Kanane/terrace2.png'},
+        {name: 'Two Room', blueprint: '/images/Kanane/tworoom.png'},
+        {name: 'Master Room', blueprint: '/images/Kanane/masterroom.png'}
+      ];
       
+    
   return (
     <div className='swiper-container'>
       <Swiper
+       onSlideChange={handleSlideChange}
       effect="coverflow"
       grabCursor={true}
       centeredSlides={true}
@@ -63,13 +56,13 @@ const Kanane_type = () => {
         clickable:true,
       }}
       initialSlide={2}
-      className="mySwiper"
+      className="TextSwiper"
     >
-      {tabs.map((tab, index) => (
-        <SwiperSlide key={index}>
-          <h2 className='types'>{tab}</h2>
-        </SwiperSlide>
-      ))}
+       {slides.map((slide, index) => (
+          <SwiperSlide key={index}>
+                <h2 className='types'>{slide.name}</h2>
+          </SwiperSlide>
+        ))}
       <div className="slider-controller">
         <div className="swiper-btn-prev slider-arrow">
             <MdArrowBackIos className='arrow-back'></MdArrowBackIos>
@@ -79,6 +72,13 @@ const Kanane_type = () => {
         </div>
       </div>
     </Swiper>
+    <div className="blueprint-container">
+        <img 
+          src={activeSlide.blueprint} 
+          alt={`${activeSlide.name} blueprint`} 
+          className="blueprint-image"
+        />
+    </div>
     </div>
   );
 }
